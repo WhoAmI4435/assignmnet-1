11898e0767def2c3560baf7b8cc7bd8f35b32075
WhoAmI4435 <discordidandadi@gmail.com>
2025-07-02 01:23:20 +0530

    initial commit

---
+++ task-1.py
@@ -0,0 +1,9 @@
+a = float(input("enter number 1: "))
+b = float(input("enter number 2: "))
+
+print("\n\n")
+
+print("addition: ", a+b)
+print("subtraction: ", a-b)
+print("multiplication: ", a*b)
+print("division: ", a/b)
